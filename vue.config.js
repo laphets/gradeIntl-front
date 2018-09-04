95b0ef58ffb48cbee328fa2a06fc092c814156fe
laphets <i@laphets.com>
2018-09-04 18:08:06 +0800

    Update env

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     port: 80,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:8290',
         pathRewrite: {
           '^/api': '/'
         }
